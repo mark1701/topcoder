b5a60710e5023513a8a85a2b5e9bef0eea22165e
marco <marco.passuello@gmail.com>
2020-12-10 17:24:04 +0000

    added solution for KilomanX

--- problems/KiloManX/KiloManX.java
+++ problems/KiloManX/KiloManX.java
@@ -3,6 +3,51 @@ import java.util.*;
 public class KiloManX{
 	
 	public int leastShots(String[] damageChart, int[] bossHealth){
-		return 5;
+		
+		int N = damageChart.length;
+		boolean[] visited = new boolean[(int)Math.pow(2,N)];
+		
+		int[][] chart = new int[N][N];
+		for(int i = 0; i < N; i++)
+			for(int j = 0; j < N; j++)
+				chart[i][j] = damageChart[i].charAt(j)-'0';
+		
+		PriorityQueue<Node> queue = new PriorityQueue<>(11, (a,b) -> a.hits - b.hits);
+		queue.add(new Node(0,0));
+		
+		while(!queue.isEmpty()){
+			Node node = queue.poll();
+			if(node.weapons == Math.pow(2,N)-1) return node.hits;
+
+			if(visited[node.weapons]) continue;
+			visited[node.weapons] = true;
+			
+			for(int i = 0; i < N; i++){
+				int minHits = bossHealth[i];
+				if(((node.weapons >> i) & 1) == 1) continue;
+				for(int j = 0; j < N; j++){
+					if(i == j) continue;
+					if(((node.weapons >> j) & 1) == 1 && chart[j][i] != 0) { //we have the weapon and it does damage to this boss
+						int hits = bossHealth[i]/chart[j][i];
+						double residue = bossHealth[i] % chart[j][i];
+						if(residue > 0) hits++;
+						minHits = Math.min(minHits, hits);
+					}					
+				}
+				Node next = new Node((node.weapons | (1 << i)),node.hits+minHits);
+				queue.add(next);
+			}
+			
+		}
+		return 0;				
+	}
+
+	private class Node{
+		public int weapons, hits;
+
+		public Node(int w, int s){
+			weapons = w;
+			hits = s;
+		}
 	}
 }
--- problems/KiloManX/Test.java
+++ problems/KiloManX/Test.java
@@ -4,6 +4,10 @@ public class Test {
 
 	public static void main(String[] args){
 		runTest(new String[] {"070","500","140"}, new int[] {150,150,150}, 218);
+		runTest(new String[] {"1542", "7935", "1139", "8882"}, new int[] {150,150,150,150}, 205);		
+		runTest(new String[] {"07", "40"}, new int[] {150,10}, 48);
+		runTest(new String[] {"198573618294842", "159819849819205", "698849290010992", "000000000000000", "139581938009384", "158919111891911", "182731827381787", "135788359198718", "187587819218927", "185783759199192", "857819038188122", "897387187472737", "159938981818247", "128974182773177", "135885818282838"}, new int[] {157, 1984, 577, 3001, 2003, 2984, 5988, 190003, 9000, 102930, 5938, 1000000, 1000000, 5892, 38}, 260445);
+		runTest(new String[] {"02111111", "10711111", "11071111", "11104111", "41110111", "11111031", "11111107", "11111210"}, new int[] {28,28,28,28,28,28,28,28}, 92);
 	}
 
 	private static boolean runTest(String[] weapons,  int[] health, int expectedResult){
@@ -12,7 +16,7 @@ public class Test {
 		int result = instance.leastShots(weapons, health);
 		
 		if(result != expectedResult) {				
-			System.out.println("Error: getting " + result + " vs " + expectedResult);
+			System.out.println("Error: getting " + result + " instead of " + expectedResult);
 			return false;
 		}
 		return true;
